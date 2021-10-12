db096a55dd21d0ed2b89ed5f5baf60f371977f4d
Chung <huachung1304@gmail.com>
2021-10-12 11:27:20 +0700

    update code 12/10/2021

--- controllers/UsersControllers.js
+++ controllers/UsersControllers.js
@@ -10,7 +10,24 @@ const getAllUser = () => {
   return KhachHang.findAll();
 };
 
+// Lấy user theo taiKhoan
+const getUserByTaiKhoan = (taiKhoan) => {
+  return KhachHang.findOne({ where: { taiKhoan: taiKhoan } })
+}
+// Xóa user
+const deleteUser = (taiKhoan) => {
+  return KhachHang.destroy({ where: { taikhoan: taiKhoan } })
+};
+
+// Cập nhật user
+const updateUser = (taiKhoan, dataUser) => {
+  return KhachHang.update(dataUser, { where: { taiKhoan: taiKhoan } })
+}
+
 module.exports = {
   createUser,
   getAllUser,
+  getUserByTaiKhoan,
+  deleteUser,
+  updateUser,
 };
--- routers/UsersRouter.js
+++ routers/UsersRouter.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { RESPONSE_CODE } = require("../constants");
 const bcryptjs = require("bcryptjs");
-const { createUser, getAllUser } = require("../controllers/UsersControllers");
+const { createUser, getAllUser, deleteUser, updateUser, getUserByTaiKhoan } = require("../controllers/UsersControllers");
 const UsersRouter = express.Router();
 
 UsersRouter.post("/ThemNguoiDung", async (req, res) => {
@@ -70,6 +70,82 @@ UsersRouter.get("/LayDanhSachNguoiDung", async (req, res) => {
     res.status(RESPONSE_CODE.INTERNAL_SERVER_ERROR).send(err);
   }
 });
+
+
+// Xóa User
+UsersRouter.delete("/XoaNguoiDung", async (req, res) => {
+  try {
+    const { taiKhoan = "" } = req.query;
+    // Kiểm tra giá trị taiKhoan
+    const userList = await getAllUser();
+    const findTaiKhoan = userList.findIndex(user => user.taiKhoan === taiKhoan);
+    if (findTaiKhoan === -1) {
+      res.status(RESPONSE_CODE.BAD_REQUEST).send(`Tài khoản ${taiKhoan} không tồn tại`)
+    } else {
+      await deleteUser(taiKhoan);
+      res.status(RESPONSE_CODE.OK).send(`Xóa thành công người dùng ${taiKhoan}`)
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(RESPONSE_CODE.INTERNAL_SERVER_ERROR).send(err)
+  }
+})
+
+
+// Cập nhật user (dành cho người dùng)
+UsersRouter.put("/SuaThongTinCaNhan", async (req, res) => {
+  try {
+    const {
+      taiKhoan,
+      ho,
+      ten,
+      email,
+      gioiTinh,
+      ngaySinh,
+      diaChi,
+      soDT,
+      matKhau,
+      avatar,
+    } = req.body;
+    const dataUser = {
+      taiKhoan,
+      ho,
+      ten,
+      email,
+      gioiTinh,
+      ngaySinh,
+      diaChi,
+      soDT,
+      matKhau,
+      avatar,
+      loaiKH: false,
+      loaiND: false,
+    }
+    // Kiểm tra tài khoản 
+    const getUserList = await getAllUser()
+    const findTaiKhoanUser = getUserList.findIndex(user => user.taiKhoan === dataUser.taiKhoan)
+    const findEmailUser = getUserList.findIndex(user => user.email === dataUser.email)
+    console.log(findEmailUser)
+    if (findTaiKhoanUser === -1) {
+      res.status(RESPONSE_CODE.BAD_REQUEST).send(`Tài khoản ${dataUser.taiKhoan} không tồn tại `)
+    }
+    else {
+      const getOneUser = await getUserByTaiKhoan(dataUser.taiKhoan);
+      if (findEmailUser === -1 || getOneUser.email === dataUser.email) {
+        await updateUser(dataUser.taiKhoan, dataUser);
+        res.status(RESPONSE_CODE.OK).send(`Cập nhật thành công người dùng ${dataUser.taiKhoan}`)
+      }
+      else {
+        res.status(RESPONSE_CODE.BAD_REQUEST).send("Email đã tồn tại")
+      }
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(RESPONSE_CODE.INTERNAL_SERVER).send(err)
+  }
+})
+
+
 
 module.exports = {
   UsersRouter,
